eadb4c3d0fb44d835fed015e0e9bd1706a21e7bc
bernardosilva162007 <bernardosilva162007@gmail.com>
2025-11-15 16:50:44 +0000

    Adiciona a leitura dos 14 numeros com validacao

--- Include/functions-team-7.c
+++ Include/functions-team-7.c
@@ -1,3 +1,31 @@
+#include <stdio.h>
 #include "functions-team-7.h"
 
-// TODO
+// Funçao que le os numeros inteiros de 3 a 31
+void lernumeros(int vetor[], int tamanho)
+{
+    int i;
+
+    for (i = 0; i < tamanho; i++)
+    {
+        int numero, valido;
+
+        do
+        {
+            printf("Digite o numero %d (entre 3 e 31) : ", i + 1);
+            valido = scanf("%d", &numero);
+
+            while (getchar() != '\n')
+                ;
+
+            if (!valido || numero < 3 || numero > 31)
+            {
+                printf("Entrada invalida! Tente novamente. \n");
+                valido = 0;
+            }
+
+        } while (!valido);
+
+        vetor[i] = numero;
+    }
+}
