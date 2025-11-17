1e6d74d5e6dd1f6d58e2cf30eee50c82e0538283
Dani234jf <dani.fof.jf@gmail.com>
2025-11-17 10:17:46 +0000

    Implementacao da funcao que mostra o menu e executa as funcoes de cada opcao

--- Include/functions-team-7.c
+++ Include/functions-team-7.c
@@ -29,3 +29,51 @@ void lernumeros(int vetor[], int tamanho)
         vetor[i] = numero;
     }
 }
+
+// Mostrar o menu ao utilizador
+void mostarMenu()
+{
+    printf("\nMenu Calculo de estatisticas\n");
+    printf("----------------------------\n");
+    printf("1. Calculo da raiz quadrada\n2. Calculo da media\n3. Valores divisiveis por 3\n4. Ordernar de forma descendente\n");
+    printf("5. Permutacoes do vetor\n6. Valor mais proximo de 25\n");
+    printf("----------------------------\n");
+}
+
+// Executar a funcao escolhida pelo numero da opcao escolhida
+void escolherOperacao()
+{
+    int n = 0;
+    do {
+        printf("\nDigite o numero da opcao (ex: 2): ");
+        int valido = scanf("%d",&n);
+
+        if(!valido || n < 1 || n > NUMERO_DE_OPERACOES)
+        {
+            while (getchar() != '\n');
+            n = 0;
+            printf("Entrada invalida! Tente novamente.\n");
+            continue;
+        }
+        
+        switch (n)
+        {            
+            case 1:
+                break;
+            case 2:
+                break;
+            case 3:
+                break;
+            case 4:
+                break;
+            case 5:
+                break;
+            case 6:
+                break;
+            default:
+                printf("Erro: valor invalido");
+                break;
+        }
+
+    } while (n == 0);
+}
